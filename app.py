19074ffd929fd2215c20cb0004741564351d42d7
dev-nikhil02 <nikhilagarwal6271@gmail.com>
2025-09-02 21:22:33 +0530

    changed ui

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from rag_pipeline import run
 
 st.title("Agentic RAG System Demo")
 
+st.write(
+    "This demo showcases a Retrieval-Augmented Generation (RAG) system build for Q&A on documents. "
+    "Enter your financial or stock-related question on googel below, and the agent will retrieve relevant context and generate an answer."
+    "Vector db of the document is stored at backend for semantic search and retrievel."
+    "This model mainly focus on contextual retrieval from confidential data."
+)
 user_query = st.text_input("Enter your query:")
 
 if st.button("Get Answer") and user_query:
